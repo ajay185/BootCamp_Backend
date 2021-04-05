2b1659a98f6286dd27d6dbfb05a81dd518f3356a
ajay185 <ajay185@outlook.com>
2021-04-05 01:09:33 -0500

    adding terraform

--- replace.sh
+++ replace.sh
@@ -1,1 +1,4 @@
-sed "s/tagversion/$1/g" backend.yaml > backend.yml
+pwd
+cd terraform/modules/k8s
+sed "s/tagversion/$1/g" backend.tf > backend1.tf
+rm backend.tf
---
+++ terraform/var.tf
@@ -0,0 +1,18 @@
+variable "sp_subscription_id" {
+  default = "53a11754-6b0f-4541-a33f-235dd214556e"
+}
+
+variable "sp_client_id" {
+  default = "1c94acf5-d217-43ef-b407-0cd839aed413"
+}
+
+variable "sp_client_secret" {
+  default = "ZrkTUXn.2HK663a8cW-saGzYuv.UIlMz~p"
+}
+
+variable "sp_tenant_id" {
+  default = "ce3ad5a3-eeab-486e-baf2-a914a923c99c"
+}
+
+
+ 
